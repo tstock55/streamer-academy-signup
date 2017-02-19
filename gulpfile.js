e21307942f715f18d78573e82a01962c318290e1
Trevor <tstock55@gmail.com>
2017-02-18 22:26:12 -0500

    fixed gulp build dest

--- gulpfile.js
+++ gulpfile.js
@@ -53,10 +53,10 @@ gulp.task('minify-js', function() {
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+        .pipe(gulp.dest('public/vendor/bootstrap'))
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+        .pipe(gulp.dest('public/vendor/jquery'))
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -66,7 +66,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+        .pipe(gulp.dest('public/vendor/font-awesome'))
 })
 
 // Run everything
